e916d4d64b695808de8b6fcbd1c9391eaf840c4f
Subhajit Guchhait <74090703+imsubhajit98@users.noreply.github.com>
2023-02-20 15:52:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,40 +1,19 @@
-//your code here
-let input = document.getElementById('input');
-
-let expression = "";
-
-function solve(e) {
-
-    // console.log(e)
-
-    if (e === 'ans') {
-
-        // evaluate the expression
-
-        let result = eval(expression);
-
-        console.log(result);
-
-        input.value = result;
-
+let string = "";
+let buttons = document.querySelectorAll('.button');
+Array.from(buttons).forEach((button)=>{
+  button.addEventListener('click', (e)=>{
+    if(e.target.innerHTML == '='){
+      string = eval(string);
+      document.querySelector('input').value = string;
     }
-
-    else if (e === 'clear') {
-
-        expression = "";
-
-        input.value = ""
-
+    else if(e.target.innerHTML == 'C'){
+      string = ""
+      document.querySelector('input').value = string;
     }
-
-    else {
-
-        expression += e;
-
-        console.log(expression)
-
-        input.value = expression;
-
-    }
-
-}
+    else{ 
+    console.log(e.target)
+    string = string + e.target.innerHTML;
+    document.querySelector('input').value = string;
+      }
+  })
+})
